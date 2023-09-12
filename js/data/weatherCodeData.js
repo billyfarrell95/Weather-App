2466e13dafb02fbb1f648baaabda22dec8bd2e41
billyfarrell95 <billyfarrell95@gmail.com>
2023-09-12 13:59:47 -0500

    create weathercode data file

---
+++ js/data/weatherCodeData.js
@@ -0,0 +1,33 @@
+const weatherCodeData = [
+    { number: 0, value: "Sunny", src: "/assets/weather-icons/clear-sky.svg" }, // Clear sky
+    { number: 1, value: "Mostly Sunny", src: "/assets/weather-icons/mainly-clear.svg" }, // Mainly clear
+    { number: 2, value: "Partly Cloudy", src: "/assets/weather-icons/partly-cloudy.svg"  }, // Partly cloudy
+    { number: 3, value: "Overcast", src: "/assets/weather-icons/overcast.svg"  }, // Overcast
+    { number: 45, value: "Foggy", src: "/assets/weather-icons/fog.svg"  }, // Fog
+    { number: 48, value: "Freezing Fog", src: "/assets/weather-icons/fog-rime.svg"  }, // Depositing Rime Fog
+    { number: 51, value: "Light Drizzle", src: "/assets/weather-icons/drizzle-light.svg"  }, // Drizzle: Light intensity
+    { number: 53, value: "Moderate Drizzle", src: "/assets/weather-icons/drizzle-moderate.svg"  }, // Drizzle: Moderate intensity
+    { number: 55, value: "Dense Drizzle", src: "/assets/weather-icons/drizzle-dense.svg"  }, // Drizzle: Dense intensity
+    { number: 56, value: "Light Freezing Drizzle", src: "/assets/weather-icons/drizzle-freezing-light.svg"  }, // Freezing Drizzle: Light intensity
+    { number: 57, value: "Dense Freezing Drizzle", src: "/assets/weather-icons/drizzle-freezing-dense.svg"  }, // Freezing Drizzle: Dense intensity
+    { number: 61, value: "Light Rain", src: "/assets/weather-icons/rain-slight.svg"  }, // Rain: Slight intensity
+    { number: 63, value: "Moderate Rain", src: "/assets/weather-icons/rain-moderate.svg"  }, // Rain: Moderate intensity
+    { number: 65, value: "Heavy Rain", src: "/assets/weather-icons/rain-heavy.svg"  }, // Rain: Heavy intensity
+    { number: 66, value: "Light Freezing Rain", src: "/assets/weather-icons/rain-freezing-light.svg"  }, // Freezing Rain: Light intensity
+    { number: 67, value: "Heavy Freezing Rain", src: "/assets/weather-icons/rain-freezing-heavy.svg"  }, // Freezing Rain: Heavy intensity
+    { number: 71, value: "Light Snow Fall", src: "/assets/weather-icons/snow-slight.svg"  }, // Snow fall: Slight intensity
+    { number: 73, value: "Moderate Snow Fall", src: "/assets/weather-icons/snow-moderate.svg"  }, // Snow fall: Moderate intensity
+    { number: 75, value: "Heavy Snow Fall", src: "/assets/weather-icons/snow-heavy.svg"  }, // Snow fall: Heavy intensity
+    { number: 77, value: "Light Snow (icy)", src: "/assets/weather-icons/snow-grains.svg" },
+    { number: 80, value: "Light Rain Showers", src: "/assets/weather-icons/rain-shower-slight.svg"  }, // Rain showers: Slight intensity
+    { number: 81, value: "Moderate Rain Showers", src: "/assets/weather-icons/rain-shower-moderate.svg"  }, // Rain showers: Moderate intensity
+    { number: 82, value: "Heavy Rain Showers", src: "/assets/weather-icons/rain-shower-violent.svg"  }, // Rain showers: Violent intensity
+    { number: 85, value: "Light Snow Showers", src: "/assets/weather-icons/snow-shower-slight.svg"  }, // Snow showers: Slight intensity
+    { number: 86, value: "Heavy Snow Showers", src: "/assets/weather-icons/snow-shower-heavy.svg"  }, // Snow showers: Heavy intensity
+    { number: 95, value: "Thunderstorms", src: "/assets/weather-icons/thunderstorm.svg"  }, // Thunderstorm: Slight or moderate
+    { number: 96, value: "Thunderstorm with Hail", src: "/assets/weather-icons/thunderstorm-slight-hail.svg"  }, // Thunderstorm with slight hail
+    { number: 99, value: "Thunderstorm with Heavy Hail", src: "/assets/weather-icons/thunderstorm-heavy-hail.svg"  } // Thunderstorm with heavy hail
+    ];
+
+
+export { weatherCodeData };
--- js/utils/processWeatherCodes.js
+++ js/utils/processWeatherCodes.js
@@ -1,36 +1,7 @@
-function processWeatherCodes(code) {
-    const weatherCodeValues = [
-        { number: 0, value: "Clear sky" },
-        { number: 1, value: "Mainly clear" },
-        { number: 2, value: "Partly cloudy" },
-        { number: 3, value: "Overcast" },
-        { number: 45, value: "Fog" },
-        { number: 48, value: "Depositing Rime Fog" },
-        { number: 51, value: "Drizzle: Light intensity" },
-        { number: 53, value: "Drizzle: Moderate intensity" },
-        { number: 55, value: "Drizzle: Dense intensity" },
-        { number: 56, value: "Freezing Drizzle: Light intensity" },
-        { number: 57, value: "Freezing Drizzle: Dense intensity" },
-        { number: 61, value: "Rain: Slight intensity" },
-        { number: 63, value: "Rain: Moderate intensity" },
-        { number: 65, value: "Rain: Heavy intensity" },
-        { number: 66, value: "Freezing Rain: Light intensity" },
-        { number: 67, value: "Freezing Rain: Heavy intensity" },
-        { number: 71, value: "Snow fall: Slight intensity" },
-        { number: 73, value: "Snow fall: Moderate intensity" },
-        { number: 75, value: "Snow fall: Heavy intensity" },
-        { number: 77, value: "Snow grains" },
-        { number: 80, value: "Rain showers: Slight intensity" },
-        { number: 81, value: "Rain showers: Moderate intensity" },
-        { number: 82, value: "Rain showers: Violent intensity" },
-        { number: 85, value: "Snow showers: Slight intensity" },
-        { number: 86, value: "Snow showers: Heavy intensity" },
-        { number: 95, value: "Thunderstorm: Slight or moderate" },
-        { number: 96, value: "Thunderstorm with slight hail" },
-        { number: 99, value: "Thunderstorm with heavy hail" }
-      ];
+import { weatherCodeData } from "../data/weatherCodeData.js";
 
-      const weathercode = weatherCodeValues.find(item => item.number == code)
+function processWeatherCodes(code) {
+      const weathercode = weatherCodeData.find(item => item.number == code)
 
       if (weathercode) {
         return weathercode.value;
